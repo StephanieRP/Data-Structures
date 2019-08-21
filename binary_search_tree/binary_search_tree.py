4304ab65df38ecc31e6049210f550a0d69db1f63
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-08-21 18:37:38 -0500

    added insert for binarysearchtree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,30 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        curr_node = self
+        while(True == True):
+            if(value > curr_node.value):
+                if curr_node.right != None:
+                    curr_node = curr_node.right
+                else:
+                    curr_node.right = BinarySearchTree(value)
+                    break
+            else:
+                if curr_node.left != None:
+                    curr_node = curr_node.left
+                else:
+                    curr_node.left = BinarySearchTree(value)
+                    break
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def for_each(self, cb):
-    pass
+    def for_each(self, cb):
+        pass
