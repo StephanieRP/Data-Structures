f8063069020fc622c5c59d3dc0a3cbae95d4b6e2
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-08-22 16:07:33 -0500

    start max_heap file

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,47 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        if len(self.storage) > 1:
+            self._bubble_up(len(self.storage)-1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        value = self.storage[0]
+        self.storage[0] = self.storage[len(self.storage)-1]
+        self.storage.pop(len(self.storage)-1)
+        if len(self.storage) > 0:
+            self._sift_down(0)
+        return value
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        children = []
+        childrenIndices = [2*(index+1)-1, 2*(index+1)]
+        parent = self.storage[index]
 
-  def _sift_down(self, index):
-    pass
+        if childrenIndices[0] > self.get_size()-1:
+            pass
+        else:
+            if childrenIndices[1] > self.get_size()-1:
+                childrenIndices.pop(1)
+            for childIndex in childrenIndices:
+                children.append(self.storage[childIndex])
+        if len(children) > 1:
+            if children[0] < children[1]:
+                children.reverse()
+                childrenIndices.reverse()
+
+        if children[0] > parent:
+            self.storage[index] = children[0]
+            self.storage[childrenIndices[0]] = parent
+            self._sift_down(childrenIndices[0])
+
+    def _sift_down(self, index):
+        pass
